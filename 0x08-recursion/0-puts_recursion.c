4e062380d1fe46d8fd2582de4e8253bebf1e1b90
Jibril <yjibril28@gmail.com>
2023-06-06 23:22:12 -0700

    C recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,15 @@
 #include "main.h"
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
- */
+* _0-puts_recursion code
+* return = 0
+*/
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-
-	else
-		_putchar('\n');
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
 }
--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- *
- * Return: The length of the string.
- */
+* _strlen_recursion - Returns the length properties  of a string.
+* @s: The string is to be measured or calculated.
+*
+* Return: str lenght.
+*/
 int _strlen_recursion(char *s)
 {
-	int longit = 0;
-
-	if (*s)
-	{
-		longit++;
-		longit += _strlen_recursion(s + 1);
-	}
-
-	return (longit);
+int longit = 0;
+if (*s)
+{
+longit++;
+longit += _strlen_recursion(s + 1);
+}
+return (longit);
 }
